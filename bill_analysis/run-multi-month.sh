ac507dd48dad498e64934e86ec572abfff273820
Pieter Smit <pieter.smit@twgroup.co.nz>
2025-06-11 17:54:48 +1200

    feat: bill_analysis/run-multi-month.sh fix end date calc add space to output

--- bill_analysis/run-multi-month.sh
+++ bill_analysis/run-multi-month.sh
@@ -7,14 +7,34 @@ start_year=2024
 start_month=03
 
 # Ending year and month
-end_year=2025
-end_month=04
+# Detect OS
+OS=$(uname -s)
+
+# Get year and previous month as a number
+if [ "$OS" = "Linux" ]; then
+    # Linux (GNU date)
+    end_month=$(date --date="last month" +%m)
+    end_year=$(date --date="last month" +%Y)
+elif [ "$OS" = "Darwin" ]; then
+    # macOS (BSD date)
+    end_month=$(date -v-1m +%m)
+    end_year=$(date -v-1m +%Y)
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
 
 # Convert to a comparable number (YYYYMM)
-current=$((start_year * 100 + start_month))
+start=$((start_year * 100 + start_month))
 end=$((end_year * 100 + end_month))
+current=$start
 
 cargo build --release
+
+echo
+echo "# Processing months from start=$start to end=$end"
+echo '"Total cost","res_save","res_unused","no_reservation","err"'
+# Loop through each month from start to end
 while [ $current -le $end ]; do
     # Format the date as YYYYMM
     year=$((current / 100))
@@ -26,16 +46,17 @@ while [ $current -le $end ]; do
     ./target/release/bill_analysis --bill-path "./csv_data/$formatted_date" | \
         grep "Total cost NZ" | \
         grep -v "date:" | \
-awk -F'NZ\\$' -v date="$formatted_date" '{
+        awk -F'NZ\\$' -v date="$formatted_date" '{
             # Clean each field: remove commas, labels, and extra characters
             gsub(/[,=]/, "", $2); gsub(/[^0-9.]/, "", $2);  # Total cost
             gsub(/[,=]/, "", $3); gsub(/[^0-9.]/, "", $3);  # res_save
             gsub(/[,=]/, "", $4); gsub(/[^0-9.]/, "", $4);  # res_unused
             gsub(/[,=]/, "", $5); gsub(/[^0-9.]/, "", $5);  # no_reservation
             gsub(/[,=]/, "", $6); gsub(/[^0-9.]/, "", $6);  # err
-            print date "," $2 "," $3 "," $4 "," $5 "," $6
-        }' | \
-        sed 's/ //g'
+            print date ", " $2 ", " $3 ", " $4 ", " $5 ", " $6
+        }' 
+        # | \
+        # sed 's/ //g'
     
     # echo "Finished processing $formatted_date exited with code $?"
 
@@ -47,4 +68,4 @@ awk -F'NZ\\$' -v date="$formatted_date" '{
     fi
     current=$((year * 100 + month))
 done
-echo "All months processed. end=$current"
+echo "# All months processed. start=$start to end=$end"
